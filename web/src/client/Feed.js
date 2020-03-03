5b8ebfca17252ee66bd4805c2e5cd31383574973
Waldo Valenzuela <waldo.valenzuela@istb.unibe.ch>
2020-03-03 21:43:31 +0100

    - The Apollo Query Component

--- web/src/client/Feed.js
+++ web/src/client/Feed.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-// import { Query, Mutation } from "react-apollo";
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 const GET_POSTS = gql`
   query {
@@ -43,13 +42,6 @@ class Feed extends Component {
     }));
   };
   render() {
-    const { posts, loading, error } = this.props;
-    if (loading) {
-      return 'Loading...';
-    }
-    if (error) {
-      return error.message;
-    }
     const { postContent } = this.state;
     return (
       <div>
@@ -64,25 +56,30 @@ class Feed extends Component {
           </form>
         </div>
         <div className="feed">
-          {posts.map((post, i) => (
-            <div key={post.id} className="post">
-              <div className="header">
-                <img src={post.user.avatar} />
-                <h2>{post.user.username}</h2>
-              </div>
-              <p className="content">{post.text}</p>
-            </div>
-          ))}
+          <Query query={GET_POSTS}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return 'Loading...';
+              }
+              if (error) {
+                return error.message;
+              }
+              const { posts } = data;
+              return posts.map((post, i) => (
+                <div key={post.id} className="post">
+                  <div className="header">
+                    <img src={post.user.avatar} />
+                    <h2>{post.user.username}</h2>
+                  </div>
+                  <p className="content">{post.text}</p>
+                </div>
+              ));
+            }}
+          </Query>
         </div>
       </div>
     );
   }
 }
 
-export default graphql(GET_POSTS, {
-  props: ({ data: { loading, error, posts } }) => ({
-    loading,
-    error,
-    posts
-  })
-})(Feed);
+export default Feed;
