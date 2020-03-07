2894e8cc048b0cc6f2d0622fc548f575e3cef72e
Waldo Valenzuela <waldo.valenzuela@istb.unibe.ch>
2020-03-07 11:15:29 +0100

    Chapter 05 - Reusable Reac Components - Introducing Reat patters - Controlled components

--- web/src/client/Feed.js
+++ web/src/client/Feed.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { Query, Mutation } from 'react-apollo';
-import InfiniteScroll from 'react-infinite-scroller';
+import { Mutation } from 'react-apollo';
 
-const GET_POSTS = gql`
-  query postsFeed($page: Int, $limit: Int) {
-    postsFeed(page: $page, limit: $limit) {
-      posts {
-        id
-        text
-        user {
-          avatar
-          username
-        }
-      }
-    }
-  }
-`;
 const ADD_POST = gql`
   mutation addPost($post: PostInput!) {
     addPost(post: $post) {
@@ -33,150 +18,32 @@ const ADD_POST = gql`
 class Feed extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      postContent: '',
-      hasMore: true,
-      page: 0
-    };
+    this.textArea = React.createRef();
   }
-  handlePostContentChange = event => {
-    this.setState({ postContent: event.target.value });
-  };
-  handleSubmit = event => {
-    const self = this;
-    event.preventDefault();
-    const newPost = {
-      text: this.state.postContent
-    };
-
-    this.props
-      .addPost({
-        variables: { post: newPost }
-      })
-      .then(() => {
-        self.setState(prevState => ({ postContent: '' }));
-      });
-  };
-  loadMore = fetchMore => {
-    const self = this;
-    const { page } = this.state;
-
-    fetchMore({
-      variables: {
-        page: page + 1
-      },
-      updateQuery(previousResult, { fetchMoreResult }) {
-        if (!fetchMoreResult.postsFeed.posts.length) {
-          self.setState({ hasMore: false });
-          return previousResult;
-        }
-
-        self.setState({ page: page + 1 });
-
-        const newData = {
-          postsFeed: {
-            __typename: 'PostFeed',
-            posts: [
-              ...previousResult.postsFeed.posts,
-              ...fetchMoreResult.postsFeed.posts
-            ]
-          }
-        };
-        return newData;
-      }
-    });
-  };
   render() {
     const self = this;
-    const { postContent, hasMore } = this.state;
     return (
-      <Query query={GET_POSTS} variables={{ page: 0, limit: 5 }}>
-        {({ loading, error, data, fetchMore }) => {
-          if (loading) {
-            return 'Loading...';
-          }
-          if (error) {
-            return error.message;
-          }
-          const { postsFeed } = data;
-          const { posts } = postsFeed;
-          return (
-            <div className="container">
-              <div className="postForm">
-                <Mutation
-                  mutation={ADD_POST}
-                  update={(store, { data: { addPost } }) => {
-                    const variables = { page: 0, limit: 5 };
-                    const data = store.readQuery({
-                      query: GET_POSTS,
-                      variables
-                    });
-                    data.postsFeed.posts.unshift(addPost);
-                    store.writeQuery({ query: GET_POSTS, variables, data });
-                  }}
-                  optimisticResponse={{
-                    __typename: 'mutation',
-                    addPost: {
-                      __typename: 'Post',
-                      text: postContent,
-                      id: -1,
-                      user: {
-                        __typename: 'User',
-                        username: 'Loading...',
-                        avatar: '/public/loading.gif'
-                      }
-                    }
-                  }}>
-                  {addPost => (
-                    <form
-                      onSubmit={event => {
-                        event.preventDefault();
-                        const newPost = {
-                          text: postContent
-                        };
-
-                        addPost({
-                          variables: { post: newPost }
-                        }).then(() => {
-                          self.setState(prevState => ({ postContent: '' }));
-                        });
-                      }}>
-                      <textarea
-                        value={postContent}
-                        onChange={this.handlePostContentChange}
-                        placeholder="Write your custom post!"
-                      />
-                      <input type="submit" value="Submit" />
-                    </form>
-                  )}
-                </Mutation>
-              </div>
-              <div className="feed">
-                <InfiniteScroll
-                  loadMore={() => self.loadMore(fetchMore)}
-                  hasMore={hasMore}
-                  loader={
-                    <div className="loader" key={'loader'}>
-                      Loading ...
-                    </div>
-                  }>
-                  {posts.map((post, i) => (
-                    <div
-                      key={post.id}
-                      className={'post ' + (post.id < 0 ? 'optimistic' : '')}>
-                      <div className="header">
-                        <img src={post.user.avatar} />
-                        <h2>{post.user.username}</h2>
-                      </div>
-                      <p className="content">{post.text}</p>
-                    </div>
-                  ))}
-                </InfiniteScroll>
-              </div>
-            </div>
-          );
-        }}
-      </Query>
+      <div className="container">
+        <div className="postForm">
+          <Mutation mutation={ADD_POST}>
+            {addPost => (
+              <form
+                onSubmit={event => {
+                  event.preventDefault();
+                  addPost({
+                    variables: { post: self.textArea.current.value }
+                  });
+                }}>
+                <textarea
+                  ref={this.textArea}
+                  placeholder="Write your custom post!"
+                />
+                <input type="submit" value="Submit" />
+              </form>
+            )}
+          </Mutation>
+        </div>
+      </div>
     );
   }
 }
