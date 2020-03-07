3ac583c8fb84338182ffdfa72c042a013ee19058
Waldo Valenzuela <waldo.valenzuela@istb.unibe.ch>
2020-03-07 19:08:22 +0100

    - Efficient Apollo React components - The Apollo Query component

--- web/src/client/Feed.js
+++ web/src/client/Feed.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { Query, Mutation } from 'react-apollo';
-import InfiniteScroll from 'react-infinite-scroller';
-import Loading from './components/loading';
-import Error from './components/error';
-import Post from './components/post';
+import { Mutation } from 'react-apollo';
+import PostsFeedQuery from './components/queries/postsFeed';
+import FeedList from './components/post/feedlist';
 
 const ADD_POST = gql`
   mutation addPost($post: PostInput!) {
@@ -37,121 +35,60 @@ class Feed extends Component {
   constructor(props) {
     super(props);
     this.textArea = React.createRef();
-    this.state = {
-      page: 0,
-      hasMore: true
-    };
   }
-  loadMore = fetchMore => {
-    const self = this;
-    const { page } = this.state;
-    fetchMore({
-      variables: {
-        page: page + 1
-      },
-      updateQuery(previousResult, { fetchMoreResult }) {
-        if (!fetchMoreResult.postsFeed.posts.length) {
-          self.setState({ hasMore: false });
-          return previousResult;
-        }
-        self.setState({ page: page + 1 });
-        const newData = {
-          postsFeed: {
-            __typename: 'PostFeed',
-            posts: [
-              ...previousResult.postsFeed.posts,
-              ...fetchMoreResult.postsFeed.posts
-            ]
-          }
-        };
-        return newData;
-      }
-    });
-  };
+
   render() {
     const self = this;
-    const { hasMore } = this.state;
     return (
-      <Query query={GET_POSTS} variables={{ page: 0, limit: 5 }}>
-        {({ loading, error, data, fetchMore }) => {
-          if (loading) {
-            return <Loading />;
-          }
-          if (error) {
-            return (
-              <Error>
-                <p>{error.message}</p>
-              </Error>
-            );
-          }
-          const { postsFeed } = data;
-          const { posts } = postsFeed;
-          return (
-            <div className="container">
-              <div className="postForm">
-                <Mutation
-                  mutation={ADD_POST}
-                  update={(store, { data: { addPost } }) => {
-                    const variables = { page: 0, limit: 5 };
-                    const data = store.readQuery({
-                      query: GET_POSTS,
-                      variables
-                    });
-                    data.postsFeed.posts.unshift(addPost);
-                    store.writeQuery({ query: GET_POSTS, variables, data });
-                  }}
-                  optimisticResponse={{
-                    __typename: 'mutation',
-                    addPost: {
-                      __typename: 'Post',
-                      text: self.textArea.current
-                        ? self.textArea.current.value
-                        : '',
-                      id: -1,
-                      user: {
-                        __typename: 'User',
-                        username: 'Loading...',
-                        avatar: '/public/loading.gif'
-                      }
+      <div className="container">
+        <div className="postForm">
+          <Mutation
+            mutation={ADD_POST}
+            update={(store, { data: { addPost } }) => {
+              const variables = { page: 0, limit: 5 };
+              const data = store.readQuery({
+                query: GET_POSTS,
+                variables
+              });
+              data.postsFeed.posts.unshift(addPost);
+              store.writeQuery({ query: GET_POSTS, variables, data });
+            }}
+            optimisticResponse={{
+              __typename: 'mutation',
+              addPost: {
+                __typename: 'Post',
+                text: self.textArea.current ? self.textArea.current.value : '',
+                id: -1,
+                user: {
+                  __typename: 'User',
+                  username: 'Loading...',
+                  avatar: '/public/loading.gif'
+                }
+              }
+            }}>
+            {addPost => (
+              <form
+                onSubmit={event => {
+                  event.preventDefault();
+                  addPost({
+                    variables: {
+                      post: { text: self.textArea.current.value }
                     }
-                  }}>
-                  {addPost => (
-                    <form
-                      onSubmit={event => {
-                        event.preventDefault();
-                        addPost({
-                          variables: {
-                            post: { text: self.textArea.current.value }
-                          }
-                        });
-                      }}>
-                      <textarea
-                        ref={this.textArea}
-                        placeholder="Write your custom post!"
-                      />
-                      <input type="submit" value="Submit" />
-                    </form>
-                  )}
-                </Mutation>
-              </div>
-              <div className="feed">
-                <InfiniteScroll
-                  loadMore={() => self.loadMore(fetchMore)}
-                  hasMore={hasMore}
-                  loader={
-                    <div className="loader" key={'loader'}>
-                      Loading ...
-                    </div>
-                  }>
-                  {posts.map((post, i) => (
-                    <Post key={post.id} post={post} />
-                  ))}
-                </InfiniteScroll>
-              </div>
-            </div>
-          );
-        }}
-      </Query>
+                  });
+                }}>
+                <textarea
+                  ref={this.textArea}
+                  placeholder="Write your custom post!"
+                />
+                <input type="submit" value="Submit" />
+              </form>
+            )}
+          </Mutation>
+        </div>
+        <PostsFeedQuery>
+          <FeedList />
+        </PostsFeedQuery>
+      </div>
     );
   }
 }
