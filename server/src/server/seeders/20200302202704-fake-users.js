4be8268925c7779c0f9e6eafaf498a0212f4bc9f
Waldo Valenzuela <waldo.valenzuela@istb.unibe.ch>
2020-03-09 21:43:58 +0100

    Chapter 06 - Authentication with Apollo and React - JSON Web Tokens - localStorage versus cookie - Authentication with GraphQL

--- server/src/server/seeders/20200302202704-fake-users.js
+++ server/src/server/seeders/20200302202704-fake-users.js
@@ -8,12 +8,18 @@ module.exports = {
         {
           avatar: '/uploads/avatar1.png',
           username: 'TestUser',
+          password:
+            '$2a$10$bE3ovf9/Tiy/d68bwNUQ0.zCjwtNFq9ukg9h4rhKiHCb6x5ncKife',
+          email: 'test1@example.com',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           avatar: '/uploads/avatar2.png',
           username: 'TestUser2',
+          password:
+            '$2a$10$bE3ovf9/Tiy/d68bwNUQ0.zCjwtNFq9ukg9h4rhKiHCb6x5ncKife',
+          email: 'test2@example.com',
           createdAt: new Date(),
           updatedAt: new Date()
         }
