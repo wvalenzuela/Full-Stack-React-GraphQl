2c7585935810de53dd39835cd1b67fa7840d437b
Waldo Valenzuela <waldo.valenzuela@istb.unibe.ch>
2020-03-13 21:46:12 +0100

    - Secured routes

--- web/src/client/router.js
+++ web/src/client/router.js
@@ -8,16 +8,62 @@ import {
   Switch
 } from 'react-router-dom';
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      rest.loggedIn === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+const LoginRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      rest.loggedIn === false ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/app'
+          }}
+        />
+      )
+    }
+  />
+);
+
 class Routing extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route
+          <PrivateRoute
             path="/app"
             component={() => (
               <Main changeLoginState={this.props.changeLoginState} />
             )}
+            loggedIn={this.props.loggedIn}
+          />
+          <LoginRoute
+            exact
+            path="/"
+            component={() => (
+              <LoginRegisterForm
+                changeLoginState={this.props.changeLoginState}
+              />
+            )}
+            loggedIn={this.props.loggedIn}
           />
         </Switch>
       </Router>
